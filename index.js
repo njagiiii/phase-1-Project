d39b7fc4093776e976e53377008f13c326980739
njagiiii <grace.makena@student.moringaschool.com>
2023-07-09 15:22:19 -0400

    updated the post patch and delete crude

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
-  
+  document.getElementById('addforms').addEventListener('submit', (e) => {
+    e.preventDefault();
+      if(e.submitter.value === 'submit'){
+          //handle submit
+          const productsObj = {
+            name: e.target.name.value,
+            price: e.target.price.value,
+            image : e.target.image_url.value
+          };
+          postproducts(null,productsObj)
+          alert('product added successfully')
+      }
+        else if(e.submitter.value === 'update'){
+          //handle update
+          const patchObj = {
+            name: e.target.name.value,
+            price: e.target.price.value,
+            image : e.target.image_url.value
+          };
+          const productid = document.getElementById('id').value //get the element id
+          patchProducts(productid, patchObj);
+          alert('product updated successfully')
+        }
+    
+    
+     //reset form after submission
+     e.target.reset();
+    });
        
     //toogle the read more visibility by using event delegation in features
       const container = document.getElementById('homecontainer')
@@ -28,7 +55,9 @@ document.addEventListener('DOMContentLoaded', () => {
              }
          }
        });
-       const productcontainer = document.getElementById('productcontainer')
+
+       // fetch the products
+       
        fetch('https://code-33jd.onrender.com/products')
        .then(res => {
          if(!res.ok){
@@ -39,22 +68,22 @@ document.addEventListener('DOMContentLoaded', () => {
        .then(data => {
         const productcontent = document.getElementById('productcontent')
 
-          data.map(products => {
+          data.forEach(product => {
             const card = document.createElement('div')
             card.classList.add('card')
 
             const image = document.createElement('img')
-            image.src = products.image
+            image.src = product.image
             image.classList.add('images')
             card.appendChild(image)
 
             const name = document.createElement('p')
-            name.innerText = products.name
+            name.innerText = product.name
             name.classList.add('names')
             card.appendChild(name)
 
             const price =document.createElement('p')
-            price.innerText = `price: ${products.price}`;
+            price.innerText = `price: ${product.price}`;
             price.classList.add('price')
             card.append(price)
 
@@ -64,21 +93,24 @@ document.addEventListener('DOMContentLoaded', () => {
             card.appendChild(button)
 
             button.addEventListener('click', () => {
-              showModal(products)
+              showModal(product,card)
             })
+
+           
 
             productcontent.appendChild(card)
           })
        })
        .catch(error => console.log(error));
 
-       function showModal(products){
+       function showModal(product,card){
         const modal = document.getElementById('modal')
         const closeBtn = document.getElementsByClassName('close')[0];
         const addToCartBtn = document.getElementById('addTocart');
         const quantityInput = document.getElementById('quantity');
         const totalAmount = document.getElementById('totalamount');
-
+        const deleteBtn = document.getElementById('deletebtn')
+  
         modal.style.display='block'
 
         //close the modal when button is clickes
@@ -86,38 +118,60 @@ document.addEventListener('DOMContentLoaded', () => {
           modal.style.display='none'
         });
 
+        //delete the card image
+        deleteBtn.addEventListener('click',()=>{
+          fetch(`https://code-33jd.onrender.com/products/${product.id}`,{
+            method:'DELETE'
+          })
+          .then(res => {
+            if(!res.ok){
+              throw Error('Failed')
+            }else{
+              card.remove();
+              modal.style.display = 'none'
+              alert('product has been deleted successfully')
+            }
+          })
+          .catch(error => console.log(error))
+
+        });
+         
+
         addToCartBtn.addEventListener('click',() => {
           const quantity = parseInt(quantityInput.value);
-          const amount = products.price * quantity;
+           const amount = quantity * product.price;
           totalAmount.innerText = `Total Amount:$${amount.toFixed(2)}`;
         });
+
+        
         
        }
-        //toogle the read more visibility by using event delegation in categories
-           const categorycontainer = document.getElementById('categorycontainer')
-           const showing = [
-            document.getElementById('show1'),
-            document.getElementById('show2'),
-            document.getElementById('show3')
-           ];
-           let isShowVisible = false
 
-           categorycontainer.addEventListener('click' ,(e) => {
-            if(e.target.classList.contains('Buttons')){
-                if(isShowVisible){
-                  showing.map(show => {
-                     show.style.display='none'
-                  })
-                  isShowVisible = false
-                } 
-                else{
-                  showing.map(show=>{
-                     show.style.display = 'block'
-                  })
-                  isShowVisible = true
-                }
-            }
-          });
+       //toogle the read more visibility by using event delegation in categories
+       const category = document.getElementById('categorycontainer')
+       const showing=[
+          document.getElementById('show1'),
+          document.getElementById('show2'),
+          document.getElementById('show3')
+       ];
+        
+       let isShowVisible = false
+        category.addEventListener('click' ,(e) => {
+          if(e.target.classList.contains('Buttons')){
+              if(isShowVisible){
+                showing.map(show => {
+                   show.style.display='none'
+                })
+                isShowVisible = false
+              } 
+              else{
+                showing.map(show=>{
+                  show.style.display = 'block'
+                })
+                isShowVisible = true
+              }
+          }
+        });
           //make the review section swipe
            let swiper =new Swiper ('.review-slider',{
             loop:true,
@@ -139,52 +193,35 @@ document.addEventListener('DOMContentLoaded', () => {
               },
             },
            })
-          //  add smooth scrolling to each navbar link
-           document.querySelectorAll('nav a').forEach(link => {
-            link.addEventListener('click',(e) => {
-              e.preventDefault();
-              const targetid = link.getAttribute('href');
-              const targetSection = document.querySelector(targetid)
-
-              if(targetSection){
-                targetSection.scrollIntoView({behavior:'smooth'});
-              }
-            });
-           });
 
           
-     //get form 
-      let formss = document.getElementById('formss')
-      formss.addEventListener('submit',(e) =>{
-        e.preventDefault();
-
-        let name = document.getElementById('name').value
-        let price = document.getElementById('price').value
-        let image = document.getElementById('image').value
-        let number = document.getElementById('idnumber').value
-
-        //fetch post request
-        fetch('https://code-33jd.onrender.com/products',{
-          method:'POST',
-          body:JSON.stringify({
-            name:name,
-            price:price,
-            image : image,
-            id: number
-          }),
-          headers:{
-            "content-type":"application/json; charset=UTF-8"
-          }
-        })
-        .then(function(res){
-          return res.json()
-        })
-        .then(function(data){
-          console.log(data)
-        })
-      })
-
+     // add post,patch and delete to the form
        
+     function postproducts(productid,productsObj){
+      fetch('https://code-33jd.onrender.com/products' ,{
+        method:'POST',
+        headers:{
+          'Content-Type':'application/json'
+        },
+        body: JSON.stringify(productsObj)
+      })
+      .then(res => res.json())
+      .then(datas => console.log(datas))
+      .catch(error => console.log(error))
+    }
+
+    function patchProducts(productid, patchObj){
+      fetch(`https://code-33jd.onrender.com/products/${productid}` ,{
+        method:'PATCH',
+        headers:{
+          'Content-Type':'application/json'
+        },
+        body: JSON.stringify(patchObj)
+      })
+      .then(res => res.json())
+      .then(datas => console.log(datas))
+      .catch(error => console.log(error))
+    }
     });
 
     
